f6faaebcdc446d525acb2374ff8775131af724dd
Tushar Pawar <pawartushar1411@gmail.com>
2020-03-17 04:32:38 +0530

    Revert "Add heroku config"
    This reverts commit df9af4780ca4a2276ab334751ceaea37cd9343a6.

--- src/app.py
+++ src/app.py
@@ -1,7 +1,15 @@
-import os
+import argparse
 from server.instance import server
-
-env_name = os.environ['env'] or 'dev'
+from util.util import validate_env_name
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Read application environment')
+    parser.add_argument('--env',
+                        dest='env',
+                        default='dev',
+                        type=validate_env_name,
+                        help='Valid environment values: dev, stg and prod. Default is dev.')
+
+    args = parser.parse_args()
+    env_name = args.env
     server.run(env_name)
