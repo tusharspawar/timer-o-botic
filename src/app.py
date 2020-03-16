df9af4780ca4a2276ab334751ceaea37cd9343a6
Tushar Pawar <pawartushar1411@gmail.com>
2020-03-17 03:01:29 +0530

    Add heroku config

--- src/app.py
+++ src/app.py
@@ -1,15 +1,7 @@
-import argparse
+import os
 from server.instance import server
-from util.util import validate_env_name
+
+env_name = os.environ['env'] or 'dev'
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Read application environment')
-    parser.add_argument('--env',
-                        dest='env',
-                        default='dev',
-                        type=validate_env_name,
-                        help='Valid environment values: dev, stg and prod. Default is dev.')
-
-    args = parser.parse_args()
-    env_name = args.env
     server.run(env_name)
